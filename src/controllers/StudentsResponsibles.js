0df9774f43c15781859bb87b95c74f5524798ff4
Dimitry Mendes <dymealves@gmail.com>
2024-04-19 02:03:28 -0300

    Update StudentsResponsiblesController

--- src/controllers/StudentsResponsibles.js
+++ src/controllers/StudentsResponsibles.js
@@ -5,15 +5,21 @@ class StudentsResponsiblesController {
     async findStudentsByResponsible(req, res) {
         const { id } = req.params
 
-        const students = []
-
         const relationships = await StudentsResponsiblesModel.findStudentsByResponsible(id)
 
-        relationships.forEach(async relationship => {
-            const student = await Students.findById(relationship.student_id)
+        const studentsSearch = async () => {
+            const students = []
 
-            students.push(student)
-        })
+            for(const relationship of relationships) {
+                const student = await Students.findById(relationship.student_id)
+
+                students.push(student)
+            }
+
+            return students
+        }
+
+        const students = await studentsSearch()
 
         res.status(200).json({ students, message: "Users searched based on a responsible person successfully!" })
     }
